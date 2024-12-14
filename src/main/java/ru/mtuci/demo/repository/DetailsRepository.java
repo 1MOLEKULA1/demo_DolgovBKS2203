86dfd20948d6d553cb870d5908ea97c4305b013c
1MOLEKULA1 <128323360+1MOLEKULA1@users.noreply.github.com>
2024-12-14 14:08:46 +0300

    Delete src directory

--- src/main/java/ru/mtuci/demo/repository/DetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.mtuci.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.mtuci.demo.model.Details;
-
-@Repository
-public interface DetailsRepository extends JpaRepository<Details, Long> {
-}
